9fe8ab88a39bb4ae13a5bfd0df3afbb6ee14f4ad
haoxin <haoxinst@gmail.com>
2018-09-19 16:40:24 +0800

    KV - INIT

---
+++ kv/README.md
@@ -0,0 +1,16 @@
+
+# KV
+
+```bash
+mix new kv --module KV
+
+mix compile
+
+mix test
+# run particular test
+mix test test/kv_test.exs:5
+# fmt
+mix format
+# env
+MIX_ENV=prod mix compile
+```
---
+++ kv/config/config.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+# You can configure your application as:
+
+#     config :kv, key: :value
+
+# and access this configuration in your application as:
+
+#     Application.get_env(:kv, :key)
+
+# You can also configure a 3rd-party app:
+
+#     config :logger, level: :info
+
+#     import_config "#{Mix.env()}.exs"
