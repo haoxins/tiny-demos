61e228e14da44feb17fbf41f8efa21286446185f
haoxin <haoxinst@gmail.com>
2018-10-16 17:07:54 +0800

    kv up

--- kv/lib/kv/supervisor.ex
+++ kv/lib/kv/supervisor.ex
@@ -7,8 +7,8 @@ defmodule KV.Supervisor do
 
   def init(:ok) do
     children = [
+      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one},
       {KV.Registry, name: KV.Registry}
-      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one}
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
