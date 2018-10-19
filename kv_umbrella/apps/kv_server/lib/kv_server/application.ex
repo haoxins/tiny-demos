be4496f69b19d89293f6a3709da5635fecf46c81
haoxin <haoxinst@gmail.com>
2018-10-19 18:48:35 +0800

    comments

--- kv_umbrella/apps/kv_server/lib/kv_server/application.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server/application.ex
@@ -1,18 +1,13 @@
 defmodule KVServer.Application do
-  # See https://hexdocs.pm/elixir/Application.html
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
+
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg)
-      # {KVServer.Worker, arg},
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
