9c6ef251784761c86b1fca6bd2301145839cf07e
haoxin <haoxinst@gmail.com>
2019-05-10 17:05:09 +0800

    tokio

--- hello-tokio/src/main.rs
+++ hello-tokio/src/main.rs
@@ -4,6 +4,37 @@ use tokio::io;
 use tokio::net::TcpStream;
 use tokio::prelude::*;
 
-fn main() {
-    println!("Hello, world!");
+pub fn main() -> Result<(), Box<std::error::Error>> {
+    let addr = "127.0.0.1:5555".parse()?;
+
+    let client = TcpStream::connect(&addr)
+        .and_then(|stream| {
+            println!("created stream");
+            io::write_all(stream, "hello world\n").then(|result| {
+                println!("wrote to stream; success={:?}", result.is_ok());
+                Ok(())
+            })
+        })
+        .map_err(|err| {
+            // All tasks must have an `Error` type of `()`. This forces error
+            // handling and helps avoid silencing failures.
+            //
+            // In our example, we are only going to log the error to STDOUT.
+            println!("connection error = {:?}", err);
+        });
+
+    // Start the Tokio runtime.
+    //
+    // The Tokio is a pre-configured "out of the box" runtime for building
+    // asynchronous applications. It includes both a reactor and a task
+    // scheduler. This means applications are multithreaded by default.
+    //
+    // This function blocks until the runtime reaches an idle state. Idle is
+    // defined as all spawned tasks have completed and all I/O resources (TCP
+    // sockets in our case) have been dropped.
+    println!("About to create the stream and write to it...");
+    tokio::run(client);
+    println!("Stream has been created and written to.");
+
+    Ok(())
 }
