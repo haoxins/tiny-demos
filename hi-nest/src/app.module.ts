5ad06ff3149032c1f34eca1012fb0a6516310977
haoxin <haoxinst@gmail.com>
2019-06-03 17:04:05 +0800

    add typeorm module

--- hi-nest/src/app.module.ts
+++ hi-nest/src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common'
+
+import { TypeOrmModule } from '@nestjs/typeorm'
+
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { HeroController } from './hero/hero.controller'
 import { HeroService } from './hero/hero.service'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 8200,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nest',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, HeroController],
   providers: [AppService, HeroService],
 })
