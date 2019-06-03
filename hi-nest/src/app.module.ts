5c7e1a951dea67086ded63553bb582ff089dbdd4
haoxin <haoxinst@gmail.com>
2019-06-03 16:09:01 +0800

    run cmd - nest g service hero

--- hi-nest/src/app.module.ts
+++ hi-nest/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { HeroController } from './hero/hero.controller'
+import { HeroService } from './hero/hero.service'
 
 @Module({
   imports: [],
   controllers: [AppController, HeroController],
-  providers: [AppService],
+  providers: [AppService, HeroService],
 })
 export class AppModule {}
