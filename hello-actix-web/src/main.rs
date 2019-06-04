3425375b9f1715766541175d06600fc151bd9ea7
haoxin <haoxinst@gmail.com>
2019-06-04 15:45:41 +0800

    update to actix web 1.0

--- hello-actix-web/src/main.rs
+++ hello-actix-web/src/main.rs
@@ -1,16 +1,17 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use bytes::Bytes;
 
-fn greet(req: &HttpRequest) -> impl Responder {
+fn greet((body, req): (Bytes, HttpRequest)) -> impl Responder {
     let to = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", to)
 }
 
 fn main() {
-    server::new(|| {
-        App::new()
-            .resource("/", |r| r.f(greet))
-            .resource("/{name}", |r| r.f(greet))
+    HttpServer::new(|| {
+        App::new().service(
+            web::resource("/").route(web::get().to(greet)), // .resource("/{name}", |r| r.f(greet))
+        )
     })
     .bind("127.0.0.1:8000")
     .expect("Can not bind to port 8000")
