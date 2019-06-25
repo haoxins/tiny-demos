cbe8daaf443d7547dcf645a65b633fe2bf05c5dc
haoxin <haoxinst@gmail.com>
2019-06-25 17:39:59 +0800

    no need services

--- hi-nest/src/binding/binding.module.ts
+++ hi-nest/src/binding/binding.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common'
-import { BindingService } from './binding.service'
 import { BindingController } from './binding.controller'
 
 @Module({
-  providers: [BindingService],
   controllers: [BindingController],
 })
 export class BindingModule {}
--- hi-nest/src/topic/topic.module.ts
+++ hi-nest/src/topic/topic.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common'
 
-import { Topic } from './topic.entity'
-import { TopicService } from './topic.service'
 import { TopicController } from './topic.controller'
 
 @Module({
-  providers: [TopicService],
   controllers: [TopicController],
 })
 export class TopicModule {}
