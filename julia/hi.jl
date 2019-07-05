eab01a0662bf13d07bf4170e76753096a08a7554
haoxin <haoxinst@gmail.com>
2019-07-05 15:33:58 +0800

    julia

--- julia/hi.jl
+++ julia/hi.jl
@@ -4,3 +4,23 @@ x = 10
 δ = 0.00001
 
 sqrt(100)
+
+typemax(Int64)
+typemin(Int64)
+
+123 ⊻ 234
+
+[1,2,3] .^ 3
+
+NaN == NaN # false
+isequal(NaN, NaN) # true
+
+function ∑(x, y)
+  x + y
+end
+
+∑(4, 5)
+
+# In Julia, most operators are just functions with support for special syntax.
+
+x = (a = 1, b = 1 + 1) # Tuples
