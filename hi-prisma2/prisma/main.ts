234aa4a0fbf9d361fdd84194a1d537c2772e9b5a
haoxin <haoxinst@gmail.com>
2019-07-29 19:59:23 +0800

    update prisma2

---
+++ hi-prisma2/prisma/main.ts
@@ -0,0 +1,31 @@
+import Photon from "@generated/photon";
+
+const photon = new Photon();
+
+async function main() {
+  await photon.connect();
+
+  const newUser = await photon.users.create({
+    data: {
+      name: "Alice",
+      email: `alice_${Math.random()}@prisma.io`
+    }
+  });
+  console.log(newUser);
+
+  const allUsers = await photon.users.findMany();
+  console.log(allUsers);
+
+  const newPost = await photon.posts.create({
+    data: { title: 'Hello Prisma 2' }
+  })
+  console.log(newPost)
+
+  const allPosts = await photon.posts.findMany()
+  console.log(allPosts)
+
+
+  await photon.disconnect();
+}
+
+main();
