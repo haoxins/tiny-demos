116eb87ecc1c8065909453a5d4604b4f9f2e349a
haoxin <haoxinst@gmail.com>
2021-07-02 15:27:34 +0800

    python - use pyproject.toml and setup.cfg

--- test-python-m/demo/hi/__main__.py
+++ test-python-m/demo/hi/__main__.py
@@ -4,12 +4,9 @@ import sys
 print(f'Receive arguments: {sys.argv}')
 
 parser = argparse.ArgumentParser(description='Let us go go go.')
-parser.add_argument('--ns',
-                    help='The namespace')
-parser.add_argument('--name',
-                    help='The name')
-parser.add_argument('--version',
-                    help='The version')
+parser.add_argument('--ns', help='The namespace')
+parser.add_argument('--name', help='The name')
+parser.add_argument('--version', help='The version')
 
 args = parser.parse_args()
 print(args)
