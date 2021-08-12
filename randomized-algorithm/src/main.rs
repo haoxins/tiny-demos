18ee01d687eef99076ca25496e5b021d2bed3112
haoxin <haoxinst@gmail.com>
2021-08-12 21:02:20 +0800

    equals

--- randomized-algorithm/src/main.rs
+++ randomized-algorithm/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-  println!("Hello, randomized algorithm!");
+    let x = 1;
+    let y = 1;
+    let rx = &x;
+    let ry = &y;
+    let rrx = &rx;
+    let rry = &ry;
+    assert!(rrx <= rry);
+    assert!(rrx == rry);
+    assert!(rrx >= rry);
 }
