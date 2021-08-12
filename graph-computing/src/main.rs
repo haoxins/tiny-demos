614928d5d5705f8416094dedc81ee5a27bd2e798
haoxin <haoxinst@gmail.com>
2021-08-12 20:50:16 +0800

    ref ref ref

--- graph-computing/src/main.rs
+++ graph-computing/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-  println!("Hello, graph computing!");
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+    let p = Point { x: 1, y: 2 };
+    let r: &Point = &p;
+    let rr: &&Point = &r;
+    let rrr: &&&Point = &rr;
+    assert_eq!(rrr.x, 1);
+    assert_eq!(rrr.y, 2);
 }
