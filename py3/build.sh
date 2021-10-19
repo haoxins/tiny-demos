8a693463faf6c071ce54fe1aeb30093c212f422f
haoxin <haoxinst@gmail.com>
2021-10-19 17:32:09 +0800

    build

--- py3/build.sh
+++ py3/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env zsh
 
-python3 -m grpc_tools.protoc \
-  -I./grpcdemo/protos \
-  --python_out=./grpcdemo \
-  --grpc_python_out=./grpcdemo \
-  ./grpcdemo/protos/test.proto
+# python3 -m grpc_tools.protoc \
+#   -I./grpcdemo/protos \
+#   --python_out=./grpcdemo/protos \
+#   --grpc_python_out=./grpcdemo/protos \
+#   ./grpcdemo/protos/test.proto
+
+poetry install
+poetry run black .
+poetry build
--- py3/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-poetry install
-poetry run black .
-poetry build
-poetry run main
