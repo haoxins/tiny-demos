71d33cfeddcd09b2208eec7ab04fa0667d2ac512
haoxin <haoxinst@gmail.com>
2021-11-05 14:30:38 +0800

    PY Arrow

---
+++ pyarrow/tests/test_pyarrow.py
@@ -0,0 +1,13 @@
+import pyarrow as pa
+
+
+def test_hello_world():
+    days = pa.array([1, 12, 17, 23, 28], type=pa.int8())
+
+    months = pa.array([1, 3, 5, 7, 1], type=pa.int8())
+
+    years = pa.array([1990, 2000, 1995, 2000, 1995], type=pa.int16())
+
+    birthdays_table = pa.table([days, months, years], names=["days", "months", "years"])
+
+    print(birthdays_table)
