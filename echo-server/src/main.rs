2c76a2578e10237136af004f4373177290ef127f
haoxin <haoxinst@gmail.com>
2021-12-24 15:45:05 +0800

    Update main.rs

--- echo-server/src/main.rs
+++ echo-server/src/main.rs
@@ -1,11 +1,7 @@
-#![warn(rust_2018_idioms)]
-#![allow(elided_lifetimes_in_paths)]
-
-use std::net::TcpListener;
 use std::io;
+use std::net::TcpListener;
 use std::thread::spawn;
 
-/// Accept connections forever, spawning a thread for each one.
 fn echo_main(addr: &str) -> io::Result<()> {
     let listener = TcpListener::bind(addr)?;
     println!("listening on {}", addr);
@@ -18,8 +14,7 @@ fn echo_main(addr: &str) -> io::Result<()> {
         let mut write_stream = stream.try_clone()?;
         spawn(move || {
             // Echo everything we receive from `stream` back to it.
-            io::copy(&mut stream, &mut write_stream)
-                .expect("error in client thread: ");
+            io::copy(&mut stream, &mut write_stream).expect("error in client thread: ");
             println!("connection closed");
         });
     }
