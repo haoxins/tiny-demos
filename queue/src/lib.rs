f5d5cbbd0d3b0b9b77a3591f1b08968bdd76c378
haoxin <haoxinst@gmail.com>
2021-12-24 16:55:40 +0800

    Update lib.rs

--- queue/src/lib.rs
+++ queue/src/lib.rs
@@ -1,34 +1,24 @@
-#![allow(elided_lifetimes_in_paths)]
-
-/// A first-in, first-out queue of characters.
 pub struct Queue {
-    older: Vec<char>,   // older elements, eldest last.
-    younger: Vec<char>, // younger elements, youngest last.
+    older: Vec<char>,
+    younger: Vec<char>,
 }
 
 impl Queue {
-    /// Push a character onto the back of a queue.
     pub fn push(&mut self, c: char) {
         self.younger.push(c);
     }
 
-    /// Pop a character off the front of a queue. Return `Some(c)` if there
-    /// was a character to pop, or `None` if the queue was empty.
     pub fn pop(&mut self) -> Option<char> {
         if self.older.is_empty() {
             if self.younger.is_empty() {
                 return None;
             }
 
-            // Bring the elements in younger over to older, and put them in
-            // the promised order.
             use std::mem::swap;
             swap(&mut self.older, &mut self.younger);
             self.older.reverse();
         }
 
-        // Now older is guaranteed to have something. Vec's pop method
-        // already returns an Option, so we're set.
         self.older.pop()
     }
 }
