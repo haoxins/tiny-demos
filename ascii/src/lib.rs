93649ee6d935c656784065fc4e08f73b7d9ce387
haoxin <haoxinst@gmail.com>
2021-12-24 14:55:06 +0800

    Update lib.rs

--- ascii/src/lib.rs
+++ ascii/src/lib.rs
@@ -1,16 +1,10 @@
 mod my_ascii {
-    /// An ASCII-encoded string.
     #[derive(Debug, Eq, PartialEq)]
     pub struct Ascii(
-        // This must hold only well-formed ASCII text:
-        // bytes from `0` to `0x7f`.
         Vec<u8>,
     );
 
     impl Ascii {
-        /// Create an `Ascii` from the ASCII text in `bytes`. Return a
-        /// `NotAsciiError` error if `bytes` contains any non-ASCII
-        /// characters.
         pub fn from_bytes(bytes: Vec<u8>) -> Result<Ascii, NotAsciiError> {
             if bytes.iter().any(|&byte| !byte.is_ascii()) {
                 return Err(NotAsciiError(bytes));
@@ -19,21 +13,15 @@ mod my_ascii {
         }
     }
 
-    // When conversion fails, we give back the vector we couldn't convert.
-    // This should implement `std::error::Error`; omitted for brevity.
     #[derive(Debug, Eq, PartialEq)]
     pub struct NotAsciiError(pub Vec<u8>);
 
-    // Safe, efficient conversion, implemented using unsafe code.
     impl From<Ascii> for String {
         fn from(ascii: Ascii) -> String {
-            // If this module has no bugs, this is safe, because
-            // well-formed ASCII text is also well-formed UTF-8.
             unsafe { String::from_utf8_unchecked(ascii.0) }
         }
     }
 
-    // This must be placed inside the `my_ascii` module.
     impl Ascii {
         /// Construct an `Ascii` value from `bytes`, without checking
         /// whether `bytes` actually contains well-formed ASCII.
