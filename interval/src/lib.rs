26abbf5b4dfa4c0f023d0df68317f9eadf9a1931
haoxin <haoxinst@gmail.com>
2021-12-30 16:25:12 +0800

    Update lib.rs

--- interval/src/lib.rs
+++ interval/src/lib.rs
@@ -1,26 +1,2 @@
-#[derive(Debug, PartialEq)]
-struct Interval<T> {
-    lower: T,
-    upper: T,
-}
 
-use std::cmp::{Ordering, PartialOrd};
-
-impl<T: PartialOrd> PartialOrd<Interval<T>> for Interval<T> {
-    fn partial_cmp(&self, other: &Interval<T>) -> Option<Ordering> {
-        if self == other {
-            Some(Ordering::Equal)
-        } else if self.lower >= other.upper {
-            Some(Ordering::Greater)
-        } else if self.upper <= other.lower {
-            Some(Ordering::Less)
-        } else {
-            None
-        }
-    }
-}
-
-#[test]
-fn test() {
-    assert!(Interval { lower: 7, upper: 8 } <= Interval { lower: 7, upper: 8 });
-}
+impl<T: PartialOrd> PartialOrd<Interval<T>> for Interval<T>
