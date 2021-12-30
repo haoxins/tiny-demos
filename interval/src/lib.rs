33c004d8ed436891e9293017f8d81e271e2b3097
haoxin <haoxinst@gmail.com>
2021-12-30 16:20:29 +0800

    Update lib.rs

--- interval/src/lib.rs
+++ interval/src/lib.rs
@@ -34,17 +34,4 @@ fn test() {
     assert!(Interval { lower: 7, upper: 8 } >= Interval { lower: 0, upper: 1 });
     assert!(Interval { lower: 7, upper: 8 } <= Interval { lower: 7, upper: 8 });
     assert!(Interval { lower: 7, upper: 8 }.le(&Interval { lower: 7, upper: 8 }));
-
-    // Overlapping intervals aren't ordered with respect to each other.
-    let left = Interval {
-        lower: 10,
-        upper: 30,
-    };
-    let right = Interval {
-        lower: 20,
-        upper: 40,
-    };
-    assert!(!(left < right));
-    assert!(!(left > right));
-    assert!(!(left == right));
 }
