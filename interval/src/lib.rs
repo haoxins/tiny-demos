2db23e3e63a424fe17705e19b73e912e07f82e4a
haoxin <haoxinst@gmail.com>
2021-12-30 16:21:27 +0800

    Update lib.rs

--- interval/src/lib.rs
+++ interval/src/lib.rs
@@ -22,16 +22,6 @@ impl<T: PartialOrd> PartialOrd<Interval<T>> for Interval<T> {
 
 #[test]
 fn test() {
-    assert!(
-        Interval {
-            lower: 10,
-            upper: 20
-        } < Interval {
-            lower: 20,
-            upper: 40
-        }
-    );
-    assert!(Interval { lower: 7, upper: 8 } >= Interval { lower: 0, upper: 1 });
     assert!(Interval { lower: 7, upper: 8 } <= Interval { lower: 7, upper: 8 });
     assert!(Interval { lower: 7, upper: 8 }.le(&Interval { lower: 7, upper: 8 }));
 }
