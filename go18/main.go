997ce21dc83aa33cf651a33d20ebfb256709f759
haoxin <haoxinst@gmail.com>
2021-12-31 14:19:17 +0800

    Update main.go

--- go18/main.go
+++ go18/main.go
@@ -24,21 +24,15 @@ func SumNumbers[K comparable, V Number](m map[K]V) V {
 	return s
 }
 func main() {
-	// Initialize a map for the integer values
 	ints := map[string]int64{
 		"first":  34,
 		"second": 12,
 	}
 
-	// Initialize a map for the float values
 	floats := map[string]float64{
 		"first":  35.98,
 		"second": 26.99,
 	}
-
-	fmt.Printf("Generic Sums: %v and %v\n",
-		SumIntsOrFloats[string, int64](ints),
-		SumIntsOrFloats[string, float64](floats))
 
 	fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n",
 		SumIntsOrFloats(ints),
@@ -47,5 +41,4 @@ func main() {
 	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
 		SumNumbers(ints),
 		SumNumbers(floats))
-
 }
