cdc4c68e5893ac5f207304d683d35e39f0ae4209
haoxin <haoxinst@gmail.com>
2021-12-31 14:17:25 +0800

    1.18 Go generics

--- go18/main.go
+++ go18/main.go
@@ -2,18 +2,22 @@ package main
 
 import "fmt"
 
-// SumInts adds together the values of m.
-func SumInts(m map[string]int64) int64 {
-	var s int64
+type Number interface {
+	int64 | float64
+}
+
+// SumIntsOrFloats sums the values of map m. It supports both int64 and float64
+// as types for map values.
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
 	for _, v := range m {
 		s += v
 	}
 	return s
 }
 
-// SumFloats adds together the values of m.
-func SumFloats(m map[string]float64) float64 {
-	var s float64
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
 	for _, v := range m {
 		s += v
 	}
@@ -32,7 +36,16 @@ func main() {
 		"second": 26.99,
 	}
 
-	fmt.Printf("Non-Generic Sums: %v and %v\n",
-		SumInts(ints),
-		SumFloats(floats))
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumIntsOrFloats[string, int64](ints),
+		SumIntsOrFloats[string, float64](floats))
+
+	fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
+
+	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
+		SumNumbers(ints),
+		SumNumbers(floats))
+
 }
