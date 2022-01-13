d69fc464443b2989b2c293eccc044319d41b6e31
haoxin <haoxinst@gmail.com>
2022-01-13 21:11:55 +0800

    format

--- try-svelte-kit/src/routes/todos/_api.ts
+++ try-svelte-kit/src/routes/todos/_api.ts
@@ -1,17 +1,6 @@
 import type { EndpointOutput, Request } from '@sveltejs/kit';
 import type { Locals } from '$lib/types';
 
-/*
-	This module is used by the /todos.json and /todos/[uid].json
-	endpoints to make calls to api.svelte.dev, which stores todos
-	for each user. The leading underscore indicates that this is
-	a private module, _not_ an endpoint — visiting /todos/_api
-	will net you a 404 response.
-
-	(The data on the todo app will expire periodically; no
-	guarantees are made. Don't use it to organise your life.)
-*/
-
 const base = 'https://api.svelte.dev';
 
 export async function api(
