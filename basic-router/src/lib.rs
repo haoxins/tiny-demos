5eb66f0172b017f4662745755b34e976cd3d57b1
haoxin <haoxinst@gmail.com>
2022-01-17 15:31:01 +0800

    Update lib.rs

--- basic-router/src/lib.rs
+++ basic-router/src/lib.rs
@@ -6,41 +6,43 @@ struct Request {
     method: String,
     url: String,
     headers: HashMap<String, String>,
-    body: Vec<u8>
+    body: Vec<u8>,
 }
 
 struct Response {
     code: u32,
     headers: HashMap<String, String>,
-    body: Vec<u8>
+    body: Vec<u8>,
 }
 
 type BoxedCallback = Box<dyn Fn(&Request) -> Response>;
 
 struct BasicRouter {
-    routes: HashMap<String, BoxedCallback>
+    routes: HashMap<String, BoxedCallback>,
 }
 
 impl BasicRouter {
     // Create an empty router.
     fn new() -> BasicRouter {
-        BasicRouter { routes: HashMap::new() }
+        BasicRouter {
+            routes: HashMap::new(),
+        }
     }
 
     // Add a route to the router.
     fn add_route<C>(&mut self, url: &str, callback: C)
-        where C: Fn(&Request) -> Response + 'static
+    where
+        C: Fn(&Request) -> Response + 'static,
     {
         self.routes.insert(url.to_string(), Box::new(callback));
     }
 }
 
-
 impl BasicRouter {
     fn handle_request(&self, request: &Request) -> Response {
         match self.routes.get(&request.url) {
             None => not_found_response(),
-            Some(callback) => callback(request)
+            Some(callback) => callback(request),
         }
     }
 }
@@ -49,7 +51,7 @@ fn not_found_response() -> Response {
     Response {
         code: 404,
         headers: HashMap::new(),
-        body: b"<h1>Page not found</h1>".to_vec()
+        body: b"<h1>Page not found</h1>".to_vec(),
     }
 }
 
@@ -57,7 +59,7 @@ fn get_form_response() -> Response {
     Response {
         code: 200,
         headers: HashMap::new(),
-        body: b"<form>".to_vec()
+        body: b"<form>".to_vec(),
     }
 }
 
@@ -65,7 +67,7 @@ fn get_gcd_response(_req: &Request) -> Response {
     Response {
         code: 500,
         headers: HashMap::new(),
-        body: b"<h1>Internal server error</h1>".to_vec()
+        body: b"<h1>Internal server error</h1>".to_vec(),
     }
 }
 
@@ -74,7 +76,7 @@ fn req(url: &str) -> Request {
         method: "GET".to_string(),
         url: url.to_string(),
         headers: HashMap::new(),
-        body: vec![]
+        body: vec![],
     }
 }
 
