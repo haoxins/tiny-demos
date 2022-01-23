1cd7be088221802c76c39c1d9194128485e12af7
haoxin <haoxinst@gmail.com>
2022-01-23 17:38:50 +0800

    Update lib.rs

--- complex/src/lib.rs
+++ complex/src/lib.rs
@@ -1,20 +1,8 @@
-//! The chapter presents several different variations on how one might define
-//! arithmetic on a generic `Complex` type, so what we have here are a bunch of
-//! isolated modules, each of which defines its own `Complex` type in its own
-//! way. The `first_cut` module is the most well-developed.
-//!
-//! If you actually need a `Complex` type for real use, consider the
-//! `num_complex` crate, whose `Complex` type is incorporated into the `num`
-//! crate.
-
 macro_rules! define_complex {
     () => {
         #[derive(Clone, Copy, Debug)]
         struct Complex<T> {
-            /// Real portion of the complex number
             re: T,
-
-            /// Imaginary portion of the complex number
             im: T,
         }
     };
@@ -23,10 +11,7 @@ macro_rules! define_complex {
 mod first_cut {
     #[derive(Clone, Copy, Debug)]
     struct Complex<T> {
-        /// Real portion of the complex number
         re: T,
-
-        /// Imaginary portion of the complex number
         im: T,
     }
 
