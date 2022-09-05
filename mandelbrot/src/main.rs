e3fae174e2997a30653c6f085cbef0c55b1b59cd
haoxin <haoxinst@gmail.com>
2022-09-05 19:49:26 +0800

    Update main.rs

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,38 +1,3 @@
-fn mandelbrot_at_point(cx: f64, cy: f64, max_iters: usize) -> usize {
-    let mut z = Complex { re: 0.0, im: 0.0 };
-    let c = Complex { re: cx, im: cy };
-
-    for i in 0..=max_iters {
-        if z.norm() > 2.0 {
-            return i;
-        }
-        z = z * z + c;
-    }
-
-    max_iters
-}
-
-fn render_mandelbrot(escape_vals: Vec<Vec<usize>>) {
-    for row in escape_vals {
-        let mut line = String::with_capacity(row.len());
-        for column in row {
-            let val = match column {
-                0..2 => ' ',
-                2..5 => '.',
-                5..10 => '-',
-                10..30 => '*',
-                30..100 => '+',
-                100..200 => 'x',
-                200..400 => '$',
-                400..700 => '#',
-                _ => '%',
-            };
-
-            line.push(val);
-        }
-        println!("{}", line);
-    }
-}
 
 fn main() {
     let mandelbrot = calculate_mandelbrot(1000, 2.0, 1.0, -1.0, 1.0, 100, 24);
