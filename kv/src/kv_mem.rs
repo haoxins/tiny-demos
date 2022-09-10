52719cd88288a138d6ae616477dc174ef4c588d2
haoxin <haoxinst@gmail.com>
2022-09-11 02:15:11 +0800

    kv

--- kv/src/kv_mem.rs
+++ kv/src/kv_mem.rs
@@ -1,3 +1,18 @@
+use kvlib::KV;
+
+#[cfg(not(target_os = "windows"))]
+const USAGE: &str = "
+Usage:
+    kv_mem FILE get KEY
+    kv_mem FILE delete KEY
+    kv_mem FILE insert KEY VALUE
+    kv_mem FILE update KEY VALUE
+";
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let fname = args.get(1).expect(&USAGE);
+    let cmd = args.get(2).expect(&USAGE).as_ref();
+    let key = args.get(3).expect(&USAGE).as_ref();
+    let value = args.get(4);
 }
