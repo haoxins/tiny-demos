3c9fb447df2ea4a5d7c63068496a7708e96d110d
haoxin <haoxinst@gmail.com>
2022-09-12 01:09:26 +0800

    byteorder

--- kv/src/lib.rs
+++ kv/src/lib.rs
@@ -1,14 +1,19 @@
 use std::collections::HashMap;
-use std::file::{File, OpenOptions};
+use std::fs::{File, OpenOptions};
 use std::io;
 use std::io::prelude::*;
 use std::io::{BufReader, BufWriter, SeekFrom};
 use std::path::Path;
 
-use serde::{Deserialize, Serialize};
+use byteorder::LittleEndian;
+use byteorder::{ReadBytesExt, WriteBytesExt};
+use crc::{Crc, CRC_32_ISCSI};
+use serde_derive::{Deserialize, Serialize};
 
 type ByteString = Vec<u8>;
 type ByteStr = [u8];
+
+pub const CASTAGNOLI: Crc<u32> = Crc::<u32>::new(&CRC_32_ISCSI);
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct KeyValuePair {
@@ -41,6 +46,44 @@ impl KV {
         loop {
             let position = f.seek(SeekFrom::Current(0))?;
             let maybe_kv = KV::process_record(&mut f);
+
+            let kv = match maybe_kv {
+                Ok(kv) => kv,
+                Err(err) => match err.kind() {
+                    io::ErrorKind::UnexpectedEof => break,
+                    _ => return Err(err),
+                },
+            };
+            self.index.insert(kv.key, position);
+        }
+        Ok(())
+    }
+
+    fn process_record<R: Read>(f: &mut R) -> io::Result<KeyValuePair> {
+        let saved_checksum = f.read_u32::<LittleEndian>()?;
+        let key_len = f.read_u32::<LittleEndian>()?;
+        let val_len = f.read_u32::<LittleEndian>()?;
+        let data_len = key_len + val_len;
+
+        let mut data = ByteString::with_capacity(data_len as usize);
+
+        {
+            f.by_ref().take(data_len as u64).read_to_end(&mut data)?;
+
+            debug_assert_eq!(data.len(), data_len as usize);
+
+            let checksum = CASTAGNOLI.checksum(&data);
+            if checksum != saved_checksum {
+                panic!(
+                    "checksum mismatch {:08x} vs {:08x}",
+                    checksum, saved_checksum
+                );
+            }
+
+            let value = data.split_off(key_len as usize);
+            let key = data;
+
+            Ok(KeyValuePair { key, value })
         }
     }
 }
