7d8f88b3c9acc12ca8958bc0b8edfd2b34a3a4e7
haoxin <haoxinst@gmail.com>
2022-09-11 22:39:09 +0800

    serde

--- kv/src/lib.rs
+++ kv/src/lib.rs
@@ -1,1 +1,46 @@
-struct KV;
+use std::collections::HashMap;
+use std::file::{File, OpenOptions};
+use std::io;
+use std::io::prelude::*;
+use std::io::{BufReader, BufWriter, SeekFrom};
+use std::path::Path;
+
+use serde::{Deserialize, Serialize};
+
+type ByteString = Vec<u8>;
+type ByteStr = [u8];
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KeyValuePair {
+    pub key: ByteString,
+    pub value: ByteString,
+}
+
+#[derive(Debug)]
+pub struct KV {
+    f: File,
+    pub index: HashMap<ByteString, u64>,
+}
+
+impl KV {
+    pub fn open(path: &Path) -> io::Result<Self> {
+        let f = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(path)?;
+
+        let index = HashMap::new();
+        Ok(KV { f, index })
+    }
+
+    pub fn load(&mut self) -> io::Result<()> {
+        let mut f = BufReader::new(&self.f);
+
+        loop {
+            let position = f.seek(SeekFrom::Current(0))?;
+            let maybe_kv = KV::process_record(&mut f);
+        }
+    }
+}
