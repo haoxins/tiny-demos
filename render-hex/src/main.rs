b3cb2e384a8ad79b064d839349863815c0764fa6
haoxin <haoxinst@gmail.com>
2022-09-16 01:37:53 +0800

    Update main.rs

--- render-hex/src/main.rs
+++ render-hex/src/main.rs
@@ -1,5 +1,87 @@
 use std::env;
 
+use svg::node::element::path::{Command, Data, Position};
+use svg::node::element::{Path, Rectangle};
+use svg::Document;
+
+use crate::Operation::{Forward, Home, Noop, TurnLeft, TurnRight};
+
+use crate::Orientation::{East, North, South, West};
+
+const WIDTH: isize = 400;
+const HEIGHT: isize = WIDTH;
+
+const HOME_X: isize = HEIGHT / 2;
+const HOME_Y: isize = WIDTH / 2;
+
+const STROKE_WIDTH: usize = 5;
+
+#[derive(Debug, Clone, Copy)]
+enum Orientation {
+    North,
+    South,
+    West,
+    East,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Operation {
+    Forward(isize),
+    TurnLeft,
+    TurnRight,
+    Home,
+    Noop(u8),
+}
+
+#[derive(Debug)]
+struct Artist {
+    x: isize,
+    y: isize,
+    heading: Orientation,
+}
+
+impl Artist {
+    fn new() -> Self {
+        Artist {
+            x: HOME_X,
+            y: HOME_Y,
+            heading: North,
+        }
+    }
+
+    fn home(&mut self) {
+        self.x = HOME_X;
+        self.y = HOME_Y;
+    }
+
+    fn forward(&mut self, distance: isize) {
+        match self.heading {
+            North => self.y += distance,
+            South => self.y -= distance,
+            West => self.x += distance,
+            East => self.x -= distance,
+        };
+    }
+
+    fn turn_right(&mut self) {
+        self.heading = match self.heading {
+            North => East,
+            South => West,
+            West => North,
+            East => South,
+        };
+    }
+
+    fn turn_left(&mut self) {
+        self.heading = match self.heading {
+            North => West,
+            South => East,
+            West => South,
+            East => North,
+        };
+    }
+}
+
 fn main() {
     let args = env::args().collect::<Vec<String>>();
     let input = args.get(1).unwrap();
@@ -10,15 +92,6 @@ fn main() {
     let path_data = convert(&operations);
     let document = generate_svg(path_data);
     svg::save(save_to, &document).unwrap();
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Operation {
-    Forward(isize),
-    TurnLeft,
-    TurnRight,
-    Home,
-    Noop(usize),
 }
 
 fn parse(input: &str) -> Vec<Operation> {
@@ -40,31 +113,13 @@ fn parse(input: &str) -> Vec<Operation> {
     steps
 }
 
-#[derive(Debug)]
-struct Artist {
-    x: isize,
-    y: isize,
-    heading: Orientation,
-}
+fn convert(operations: &Vec<Operation>) -> Vec<Command> {
+    let mut turtle = Artist::new();
+    let mut path_data: Vec<Command> = vec![];
+    let start_at_home = Command::Move(Position::Absolute, (HOME_X, HOME_Y).into());
+    path_data.push(start_at_home);
 
-impl Artist {
-    fn forward(&mut self, distance: isize) {
-        match self.heading {
-            Orientation::North => self.y += distance,
-            Orientation::South => self.y -= distance,
-            Orientation::West => self.x += distance,
-            Orientation::East => self.x -= distance,
-        }
-    }
-
-    fn turn_right(&mut self) {
-        self.heading = match self.heading {
-            Orientation::North => Orientation::East,
-            Orientation::South => Orientation::West,
-            Orientation::West => Orientation::North,
-            Orientation::East => Orientation::South,
-        };
+    for op in operations {
+        match *op {}
     }
 }
-
-
