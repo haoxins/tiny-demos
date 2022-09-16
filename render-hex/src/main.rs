c71c48a014cfc5e584fcfb7deb895a10913deda8
haoxin <haoxinst@gmail.com>
2022-09-16 11:45:46 +0800

    Update main.rs

--- render-hex/src/main.rs
+++ render-hex/src/main.rs
@@ -80,6 +80,71 @@ impl Artist {
             East => North,
         };
     }
+
+    fn warp(&mut self) {
+        if self.x < 0 {
+            self.x = HOME_X;
+            self.heading = West;
+        } else if self.x > WIDTH {
+            self.x = HOME_X;
+            self.heading = East;
+        }
+
+        if self.y < 0 {
+            self.y = HOME_Y;
+            self.heading = North;
+        } else if self.y > HEIGHT {
+            self.y = HOME_Y;
+            self.heading = South;
+        }
+    }
+}
+
+fn parse(input: &str) -> Vec<Operation> {
+    let mut steps = Vec::<Operation>::new();
+    for byte in input.bytes() {
+        let step = match byte {
+            b'0' => Home,
+            b'1'..=b'9' => {
+                let distance = (byte - 0x30) as isize;
+                Forward(distance * (HEIGHT / 10))
+            }
+            b'a' | b'b' | b'c' => TurnLeft,
+            b'd' | b'e' | b'f' => TurnRight,
+            _ => Noop(byte),
+        };
+        steps.push(step);
+    }
+
+    steps
+}
+
+fn convert(operations: &Vec<Operation>) -> Vec<Command> {
+    let mut turtle = Artist::new();
+
+    let mut path_data = Vec::<Command>::with_capacity(operations.len());
+    let start_at_home = Command::Move(Position::Absolute, (HOME_X, HOME_Y).into());
+    path_data.push(start_at_home);
+
+    for op in operations {
+        match *op {
+            Home => turtle.home(),
+            Forward(distance) => turtle.forward(distance),
+            TurnLeft => turtle.turn_left(),
+            TurnRight => turtle.turn_right(),
+            Noop(byte) => {
+                eprintln!("Unknown byte: {:?}", byte);
+            }
+        };
+
+        let path_segment = Command::Line(Position::Absolute, (turtle.x, turtle.y).into());
+
+        path_data.push(path_segment);
+
+        turtle.warp();
+    }
+
+    path_data
 }
 
 fn main() {
@@ -92,34 +157,4 @@ fn main() {
     let path_data = convert(&operations);
     let document = generate_svg(path_data);
     svg::save(save_to, &document).unwrap();
-}
-
-fn parse(input: &str) -> Vec<Operation> {
-    let mut steps = Vec::<Operation>::new();
-    for byte in input.bytes() {
-        let step = match byte {
-            b'0' => Operation::Home,
-            b'1'..=b'9' => {
-                let distance = (byte - 0x30) as isize;
-                Operation::Forward(distance * (HEIGHT / 10))
-            }
-            b'a' | b'b' | b'c' => Operation::TurnLeft,
-            b'd' | b'e' | b'f' => Operation::TurnRight,
-            _ => Operation::Noop(byte as usize),
-        };
-        steps.push(step);
-    }
-
-    steps
-}
-
-fn convert(operations: &Vec<Operation>) -> Vec<Command> {
-    let mut turtle = Artist::new();
-    let mut path_data: Vec<Command> = vec![];
-    let start_at_home = Command::Move(Position::Absolute, (HOME_X, HOME_Y).into());
-    path_data.push(start_at_home);
-
-    for op in operations {
-        match *op {}
-    }
 }
