7af6fbc8e1fb5421acf093b0a98e33f0741a3b1a
haoxin <haoxinst@gmail.com>
2022-09-30 15:39:02 +0800

    gcd

--- play/src/main.rs
+++ play/src/main.rs
@@ -1,3 +1,6 @@
+use std::env;
+use std::str::FromStr;
+
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
@@ -12,7 +15,23 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
 }
 
 fn main() {
-    println!("{}", gcd(14, 15));
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
 
 #[test]
