14e314ffddae4df60517f686703e689bc87f7a44
haoxin <haoxinst@gmail.com>
2022-10-02 20:24:22 +0800

    rm

--- get-by-tcp/src/main.rs
+++ get-by-tcp/src/main.rs
@@ -1,15 +1,1 @@
-use std::io::prelude::*;
-use std::net::TcpStream;
-
-fn main() -> std::io::Result<()> {
-    let host = "www.baidu.com:80";
-
-    let mut conn = TcpStream::connect(host)?;
-
-    conn.write_all(b"GET / HTTP/1.0")?;
-    conn.write_all(b"\r\n\r\n")?;
-
     std::io::copy(&mut conn, &mut std::io::stdout())?;
-
-    Ok(())
-}
