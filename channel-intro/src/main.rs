ebd4e58b91484888fe14cf7f42ab9bab31b6e979
haoxin <haoxinst@gmail.com>
2022-10-02 23:14:12 +0800

    rayon

--- channel-intro/src/main.rs
+++ channel-intro/src/main.rs
@@ -1,16 +1,12 @@
 #[macro_use]
 extern crate crossbeam;
-
 use crossbeam::channel::unbounded;
 use std::thread;
-
 fn main() {
     let (tx, rx) = unbounded();
-
     thread::spawn(move || {
         tx.send(42).unwrap();
     });
-
     select! {
         recv(rx) -> msg => println!("Got {:?}", msg),
     }
--- macgen/src/main.rs
+++ macgen/src/main.rs
@@ -1,12 +1,9 @@
 extern crate rand;
-
 use rand::RngCore;
 use std::fmt;
 use std::fmt::Display;
-
 #[derive(Debug)]
 struct MacAddress([u8; 6]);
-
 impl Display for MacAddress {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
@@ -16,30 +13,14 @@ impl Display for MacAddress {
         )
     }
 }
-
 impl MacAddress {
     fn new() -> Self {
         let mut octets: [u8; 6] = [0; 6];
         rand::thread_rng().fill_bytes(&mut octets);
         octets[0] |= 0b_0000_0011;
-        MacAddress { 0: octets }
     }
-
     fn is_local(&self) -> bool {
         self.0[0] & 0b_0000_0010 == 0b_0000_0010
-    }
-
     fn is_unicast(&self) -> bool {
         self.0[0] & 0b_0000_0001 == 0b_0000_0001
-    }
-}
-
-fn main() {
     let mac = MacAddress::new();
-    println!("MAC: {}", mac);
-    println!("Is local: {}", mac.is_local());
-    println!("Is unicast: {}", mac.is_unicast());
-
-    assert!(mac.is_local());
-    assert!(mac.is_unicast());
-}
