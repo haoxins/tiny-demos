5c7bdecb893db8dcff4315b88920f6c32bbf7754
haoxin <haoxinst@gmail.com>
2022-10-02 23:32:50 +0800

    crossbeam

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,40 +1,22 @@
-use std::error;
-use std::fmt;
-use std::fs::File;
-use std::io;
-use std::net;
-use std::net::Ipv6Addr;
-
 #[derive(Debug)]
 enum CustomError {
     IO(io::Error),
     Parsing(net::AddrParseError),
 }
-
 impl fmt::Display for CustomError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:?}", self)
     }
-}
-
 impl error::Error for CustomError {}
-
 impl From<io::Error> for CustomError {
     fn from(err: io::Error) -> Self {
         CustomError::IO(err)
     }
-}
-
 impl From<net::AddrParseError> for CustomError {
     fn from(err: net::AddrParseError) -> Self {
         CustomError::Parsing(err)
     }
-}
-
 fn main() -> Result<(), CustomError> {
     let _f = File::open("not-exists.txt")?;
 
     let _localhost = "::1".parse::<Ipv6Addr>()?;
-
-    Ok(())
-}
--- hexdump/src/main.rs
+++
@@ -1,9 +0,0 @@
-    let mut f = File::open(&fname).expect("Unable to open file");
-    let mut buffer = [0; BYTES_PER_LINE];
-    while let Ok(_) = f.read_exact(&mut buffer) {
-        print!("[0x{:08x}] ", pos);
-        for byte in &buffer {
-            match *byte {
-                0x00 => print!(". "),
-                0xff => print!("## "),
-                _ => print!("{:02x} ", byte),
