826e65e31fdf32c89e632c810edf98fbbdb4e5c3
haoxin <haoxinst@gmail.com>
2022-10-02 21:35:01 +0800

    chrono
    clap v4
    cli app

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use num::Complex;
+
+fn square_add_loop(c: Complex<f64>) {
+    let mut z = Complex::new(0.0, 0.0);
+    loop {
+        z = z * z + c;
+    }
 }
