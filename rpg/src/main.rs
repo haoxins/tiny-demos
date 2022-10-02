22712095e6b2a07dad5cfc325338b373d5051834
haoxin <haoxinst@gmail.com>
2022-10-03 02:24:43 +0800

    rayon

--- rpg/src/main.rs
+++ rpg/src/main.rs
@@ -1,22 +1,3 @@
-use rand;
-use rand::seq::SliceRandom;
-use rand::Rng;
-
-#[derive(Debug)]
-struct Dwarf {}
-
-#[derive(Debug)]
-struct Elf {}
-
-#[derive(Debug)]
-struct Human {}
-
-#[derive(Debug)]
-enum Thing {
-    Sword,
-    Trinket,
-}
-
 trait Enchanter: std::fmt::Debug {
     fn competency(&self) -> f64;
 
@@ -36,35 +17,4 @@ trait Enchanter: std::fmt::Debug {
             *thing = Thing::Trinket {};
         }
     }
-}
-
-impl Enchanter for Dwarf {
-    fn competency(&self) -> f64 {
-        0.5
-    }
-}
-
-impl Enchanter for Elf {
-    fn competency(&self) -> f64 {
-        0.95
-    }
-}
-
-impl Enchanter for Human {
-    fn competency(&self) -> f64 {
-        0.8
-    }
-}
-
-fn main() {
-    let mut it = Thing::Sword;
-
-    let d = Dwarf {};
-    let e = Elf {};
-    let h = Human {};
-
-    let party: Vec<&dyn Enchanter> = vec![&d, &e, &h];
-    let spellcaster = party.choose(&mut rand::thread_rng()).unwrap();
-
-    spellcaster.enchant(&mut it);
 }
