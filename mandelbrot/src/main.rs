9447f78ad637f1b9198686711dc2afb50e7367b0
haoxin <haoxinst@gmail.com>
2022-10-04 23:07:49 +0800

    Update main.rs

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,8 +1,13 @@
 use num::Complex;
 
-fn square_add_loop(c: Complex<f64>) {
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex::new(0.0, 0.0);
-    loop {
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
         z = z * z + c;
     }
+
+    None
 }
