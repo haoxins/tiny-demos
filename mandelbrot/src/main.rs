67d9515e99436a9526bee4861168b30ca9dc2c9f
haoxin <haoxinst@gmail.com>
2022-10-11 00:17:32 +0800

    Update main.rs

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -12,6 +12,13 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     }
 }
 
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex::new(re, im)),
+        None => None,
+    }
+}
+
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex::new(0.0, 0.0);
     for i in 0..limit {
@@ -33,4 +40,16 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
     assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
+    assert_eq!(parse_complex(",-0.0625"), None);
 }
