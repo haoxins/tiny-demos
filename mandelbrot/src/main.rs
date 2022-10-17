a2ce5b79d3ac588670b1c512a32cebe834edf1b4
haoxin <haoxinst@gmail.com>
2022-10-18 00:01:05 +0800

    sets

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -19,6 +19,41 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
     }
 }
 
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+    Complex::new(
+        upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
+    )
+}
+
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
+}
+
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex::new(0.0, 0.0);
     for i in 0..limit {
@@ -29,6 +64,19 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     }
 
     None
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(
+        pixel_to_point(
+            (100, 200),
+            (25, 175),
+            Complex::new(-1.0, 1.0),
+            Complex::new(1.0, -1.0)
+        ),
+        Complex::new(-0.5, -0.75)
+    );
 }
 
 #[test]
