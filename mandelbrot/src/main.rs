7b4e9fb911e20e42985be16deeb89eb3f53f9123
haoxin <haoxinst@gmail.com>
2022-10-21 00:39:46 +0800

    up

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,6 +1,47 @@
+use image::codecs::png::PngEncoder;
+use image::ColorType;
 use num::Complex;
 
+use std::env;
+use std::fs::File;
 use std::str::FromStr;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        println!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+}
+
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
+    let f = File::create(filename)?;
+    let encoder = PngEncoder::new(f);
+    encoder
+        .encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::L8)
+        .unwrap();
+
+    Ok(())
+}
 
 fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     match s.find(separator) {
