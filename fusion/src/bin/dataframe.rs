1aed81148b5420d18be0a3f76d507c6bf004137a
haoxin <haoxinst@gmail.com>
2023-03-12 02:09:45 +0800

    Create dataframe.rs

---
+++ fusion/src/bin/dataframe.rs
@@ -0,0 +1,22 @@
+use ballista::prelude::*;
+use datafusion::prelude::{col, lit, ParquetReadOptions};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let config = BallistaConfig::builder()
+        .set("ballista.shuffle.partitions", "4")
+        .build()?;
+    let ctx = BallistaContext::remote("localhost", 50050, &config).await?;
+
+    let filename = "testdata/alltypes_plain.parquet";
+
+    let df = ctx
+        .read_parquet(filename, ParquetReadOptions::default())
+        .await?
+        .select_columns(&["id", "bool_col", "timestamp_col"])?
+        .filter(col("id").gt(lit(1)))?;
+
+    df.show().await?;
+
+    Ok(())
+}
