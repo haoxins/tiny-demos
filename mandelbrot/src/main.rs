db4dae229a7efc3bad71d9c89e89fd417b3a341b
haoxin <haoxinst@gmail.com>
2023-04-29 16:29:54 +0800

    Replace crossbeam with rayon
    bye bye crossbeam

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,5 @@
+use rayon::prelude::*;
+
 use image::codecs::png::PngEncoder;
 use image::ColorType;
 use num::Complex;
@@ -24,24 +26,18 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    let threads = 8;
-    let rows_per_band = bounds.1 / threads + 1;
     {
-        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
-        crossbeam::scope(|spawner| {
-            for (i, band) in bands.into_iter().enumerate() {
-                let top = rows_per_band * i;
-                let height = band.len() / bounds.0;
-                let band_bounds = (bounds.0, height);
-                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
-                let band_lower_right =
-                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
-                spawner.spawn(move |_| {
-                    render(band, band_bounds, band_upper_left, band_lower_right);
-                });
-            }
-        })
-        .unwrap();
+        let bands: Vec<(usize, &mut [u8])> = pixels.chunks_mut(bounds.0).enumerate().collect();
+
+        bands.into_par_iter().for_each(|(i, band)| {
+            let top = i;
+            // let height = band.len();
+            let band_bounds = (bounds.0, 1);
+            let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+            let band_lower_right =
+                pixel_to_point(bounds, (bounds.0, top + 1), upper_left, lower_right);
+            render(band, band_bounds, band_upper_left, band_lower_right);
+        });
     }
 
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
