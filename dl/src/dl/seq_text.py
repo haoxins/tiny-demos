aac25da656189c78da00dc29a8509dd6625b6687
haoxin <haoxinst@gmail.com>
2023-08-09 21:52:20 +0800

    text

--- dl/src/dl/seq_text.py
+++ dl/src/dl/seq_text.py
@@ -1,5 +1,6 @@
 import re
 import os.path as path
+import collections
 
 # http://d2l-data.s3-accelerate.amazonaws.com/timemachine.txt
 
@@ -39,3 +40,52 @@ class Vocab:
             tokens = []
         if reserved_tokens is None:
             reserved_tokens = []
+
+        counter = count_corpus(tokens)
+        self._token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+        self.idx_to_token = ["<unk>"] + reserved_tokens
+        self.token_to_idx = {token: idx for idx, token in enumerate(self.idx_to_token)}
+
+        for token, freq in self._token_freqs:
+            if freq < min_freq:
+                break
+            if token not in self.token_to_idx:
+                self.idx_to_token.append(token)
+                self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+    @property
+    def unk(self):
+        return 0
+
+    @property
+    def token_freqs(self):
+        return self._token_freqs
+
+
+def count_corpus(tokens):
+    if len(tokens) == 0 or isinstance(tokens[0], list):
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
+
+
+vocab = Vocab(tokens)
+print(list(vocab.token_to_idx.items())[:10])
+
+for i in [0, 10]:
+    print("# tokens:", len(tokens[i]))
+    print(tokens[i])
+    print("# indices:", len(vocab[tokens[i]]))
+    print(vocab[tokens[i]])
